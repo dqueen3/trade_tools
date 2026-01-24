221383df79e3db4e57af69ef661152b4d59407a1
kawa <kawafmm@gmail.com>
2026-01-24 16:28:30 +0900

    fix url

--- src/fetch.ts
+++ src/fetch.ts
@@ -6,7 +6,7 @@ import { evaluateText } from "./filter";
 import { appendRecord, readAllRecords, StoredRecord } from "./storage";
 import { generateReport } from "./report";
 
-const RSS_URLS = ["https://prtimes.jp/rss"];
+const RSS_URLS = ["https://prtimes.jp/main/html/rd/p/rss.xml"];
 
 const parser = new Parser();
 
